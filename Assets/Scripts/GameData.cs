64ee69f7e89c10c953b2e9960c9e8bd4a2f44bdb
E. Steev Ramsdell <steev@2d12.com>
2017-01-27 23:20:57 -0500

    Random Movements
    Critters will move completely randomly to a valid space every 9 ticks.
    Bug Fix: Cleaned up error when checking for a valid move to a
    non-existent tile.

--- Assets/Scripts/GameData.cs
+++ Assets/Scripts/GameData.cs
@@ -4,24 +4,26 @@ using UnityEngine;
 
 public class GameData
 {
-	public const float DEFAULT_LIGHT = 4.0f;
+    public const float DEFAULT_LIGHT = 4.0f;
 
-	static Dictionary <Vector2, TileData> tileMap;
+    static Dictionary <Vector2, TileData> tileMap;
 
-	static List<LightSource> lightSources;
+    static List<LightSource> lightSources;
 
-	static CharacterData activeCharacter;
+    static CharacterData activeCharacter;
 
-	public static bool isValidMove (Vector2 target)
-	{
-		if (GetTile (target).movementSpeedMultiplier != 0) {
-			return true;
-		}
-		//TODO Other conditions, like another creature in this tile?
-		return false;
-	}
+    public static bool isValidMove(Vector2 target)
+    {
+        TileData tile = GetTile(target);
+        if (tile != null && tile.movementSpeedMultiplier != 0)
+        {
+            return true;
+        }
+        //TODO Other conditions, like another creature in this tile?
+        return false;
+    }
 
-	/*static ActionTree initiativeTracker;
+    /*static ActionTree initiativeTracker;
 
 	public static void EnqueueAction (ActionDefinition a)
 	{
@@ -44,54 +46,60 @@ public class GameData
 		}
 	}*/
 
-	public static void AddLightSource (LightSource l)
-	{
-		if (lightSources == null) {
-			lightSources = new List<LightSource> ();
-		}
+    public static void AddLightSource(LightSource l)
+    {
+        if (lightSources == null)
+        {
+            lightSources = new List<LightSource>();
+        }
 
-		lightSources.Add (l);
-	}
+        lightSources.Add(l);
+    }
 
-	public static List<LightSource> GetLightSources ()
-	{
-		if (lightSources == null) {
-			lightSources = new List<LightSource> ();
-		}
-		return lightSources;
-	}
+    public static List<LightSource> GetLightSources()
+    {
+        if (lightSources == null)
+        {
+            lightSources = new List<LightSource>();
+        }
+        return lightSources;
+    }
 
-	public static void SetTile (int x, int y, TileData td)
-	{
-		if (tileMap == null) {
-			tileMap = new Dictionary<Vector2, TileData> ();
-		}
+    public static void SetTile(int x, int y, TileData td)
+    {
+        if (tileMap == null)
+        {
+            tileMap = new Dictionary<Vector2, TileData>();
+        }
 
-		tileMap [new Vector2 (x, y)] = td;
-	}
+        tileMap[new Vector2(x, y)] = td;
+    }
 
-	public static TileData GetTile (Vector2 tileCoord)
-	{
-		if (tileMap.ContainsKey (tileCoord)) {
-			return tileMap [tileCoord];
-		} else {
-			return null;
-		}
-	}
+    public static TileData GetTile(Vector2 tileCoord)
+    {
+        if (tileMap.ContainsKey(tileCoord))
+        {
+            return tileMap[tileCoord];
+        }
+        else
+        {
+            return null;
+        }
+    }
 
-	public static TileData GetTile (int x, int y)
-	{
-		Vector2 pos = new Vector2 (x, y);
-		return GetTile (pos);
-	}
+    public static TileData GetTile(int x, int y)
+    {
+        Vector2 pos = new Vector2(x, y);
+        return GetTile(pos);
+    }
 
-	public static void SetActiveCharacter (CharacterData activate)
-	{
-		activeCharacter = activate;
-	}
+    public static void SetActiveCharacter(CharacterData activate)
+    {
+        activeCharacter = activate;
+    }
 
-	public static CharacterData GetActiveCharacter ()
-	{
-		return activeCharacter;
-	}
+    public static CharacterData GetActiveCharacter()
+    {
+        return activeCharacter;
+    }
 }
